6db2b4f184ad7906da513b7602d023ba168c76f3
sameena.c.taz <sameena.c.taz@gmail.com>
2019-07-17 20:49:25 +0100

    updated master

--- src/practicePackage/Mainfile.java
+++ src/practicePackage/Mainfile.java
@@ -3,7 +3,7 @@ package practicePackage;
 public class Mainfile {
     public static void main(String[] args) {
         Bookclass b1 = new Bookclass();
-        System.out.println("1st java program");
+        System.out.println("1st java program master");
         b1.setName("Java");
         b1.setPrice(100);
         System.out.println(b1.getName());
