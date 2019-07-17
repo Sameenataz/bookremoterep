f4d4a587cd9bdfa6074ca70ae8abdb2d167058db
sameena.c.taz <sameena.c.taz@gmail.com>
2019-07-17 20:41:21 +0100

    change1branch changes

--- src/practicePackage/Mainfile.java
+++ src/practicePackage/Mainfile.java
@@ -4,10 +4,12 @@ public class Mainfile {
     public static void main(String[] args) {
         Bookclass b1 = new Bookclass();
         System.out.println("1st java program");
-        b1.setName("Java");
-        b1.setPrice(100);
+        b1.setName("Java modified");
+        b1.setPrice(500);
         System.out.println(b1.getName());
         System.out.println(b1.getPrice());
+        System.out.println("modified program");
+
 
         /*System.out.println('name of the book',b1.getName());*/
     }
